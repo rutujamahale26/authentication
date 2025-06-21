0915ddf553c074d993d6356e3684f859b9b0fad5
rutujamahale26 <rutujamahale26@gmail.com>
2025-06-21 10:18:37 +0530

    changes apply

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose"
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 export const connectDB = async() =>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect('mongodb+srv://rutujamahale39:CR8IfsjAwOmBFIvA@cluster0.xsl6cum.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         console.log('Databse connected successfully')
     }catch(error){
         console.log('Error in connecting database', error)
